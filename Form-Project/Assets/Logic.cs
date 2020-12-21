6304f19603b3d90e607fc459043daf1bff90b25e
nav-commits <53131408+nav-commits@users.noreply.github.com>
2020-12-21 18:34:45 -0500

    commit 3

---
+++ Form-Project/Assets/Logic.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logic : MonoBehaviour
+
+    
+{
+    public InputField email;
+    public InputField password;
+    public Text resultText;
+
+
+
+    Firebase.Auth.FirebaseAuth auth;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+      auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void SignUp()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email.text.ToString(), password.text.ToString()).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            resultText.text = "signed up";
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+
+
+
+
+    public void SignIn()
+    {
+        Debug.LogError("hello");
+        resultText.text = "helllo user";
+
+        auth.SignInWithEmailAndPasswordAsync(email.text.ToString(), password.text.ToString()).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            //resultText.text = "signed in";
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
